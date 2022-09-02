a40c2c21cdadb8e6d8c1ca13d299cf40d1bb08af
Dmitry Boyarsky <boyarsky.ds@gmail.com>
2022-09-02 20:56:05 +0700

    Main.java modified

--- src/main/java/com/dmboyarsky/Main.java
+++ src/main/java/com/dmboyarsky/Main.java
@@ -1,11 +1,49 @@
 package com.dmboyarsky;
 
-public class Main {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+@Command (
+        mixinStandardHelpOptions = true,
+        name = "gendiff",
+        description =  "Compare two configuration files and shows a difference."
+)
+public class Main implements Callable<Integer> {
+    /**
+     *
+     */
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String formatName;
+
+    /**
+     *
+     */
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    /**
+     *
+     */
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Override
+    public final Integer call() throws IOException {
+        System.out.println(Differ.generate(filepath1, filepath2, formatName));
+        return 0;
+    }
+
     /**
      *
      * @param args
      */
     public static void main(final String[] args) {
-        System.out.println("");
+        new CommandLine(new Main()).execute(args);
     }
 }
