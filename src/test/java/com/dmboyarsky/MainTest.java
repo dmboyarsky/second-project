3b262949200809520aeae2a83b06684da9b790c7
Dmitry Boyarsky <boyarsky.ds@gmail.com>
2022-09-02 21:09:26 +0700

    MainTest.java added

---
+++ src/test/java/com/dmboyarsky/MainTest.java
@@ -0,0 +1,20 @@
+package com.dmboyarsky;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+}
